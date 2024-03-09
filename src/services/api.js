a77cab54700b1063c8770e2f563878e78e0d41df
fabricionft <fabriciodev859@gmail.com>
2024-03-08 21:27:17 -0300

    noca url da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const local = false;
+const url = (local) ? "http://localhost:8080" : "https://back-nftbet-production.up.railway.app/"
+
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: url,
 })
 
 export default api;
